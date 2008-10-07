436bda9696eb9b7b360d3d552694885900419d5b
Brian Frank <58821032+briansfrank@users.noreply.github.com>
2008-10-07 15:32:53 -0400

    Nullable reflection tests

--- src/jfan/fan/sys/NullableType.java
+++ src/jfan/fan/sys/NullableType.java
@@ -60,8 +60,8 @@ public class NullableType
 // Fields
 //////////////////////////////////////////////////////////////////////////
 
-  private Type root;
-  private String signature;
+  final Type root;
+  final String signature;
   private Type listOf;
 
 }
