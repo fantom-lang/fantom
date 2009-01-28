2db744e3959410b495ce6b767e570d939fe39b44
Andy Frank <1316916+afrankvt@users.noreply.github.com>
2009-01-28 11:45:38 -0500

    webappClient: HttpRes.headers

--- src/webappClient/javascript/webappClient/HttpRes.js
+++ src/webappClient/javascript/webappClient/HttpRes.js
@@ -16,6 +16,9 @@ var webappClient_HttpRes = sys_Obj.extend(
   status$set: function(val) { this.status = val; },
   status: null,
 
+  headers$get: function() { return this.headers },
+  headers: new sys_Map(),
+
   content$get: function() { return this.content },
   content$set: function(val) { this.content = val; },
   content: null,
@@ -28,6 +31,16 @@ webappClient_HttpRes.make = function(req)
   {
     res.status = req.status;
     res.content = req.responseText;
+
+    var all = req.getAllResponseHeaders().split("\n");
+    for (var i=0; i<all.length; i++)
+    {
+      if (all[i].length == 0) continue;
+      var j = all[i].indexOf(":");
+      var k = sys_Str.trim(all[i].substr(0, j));
+      var v = sys_Str.trim(all[i].substr(j+1));
+      res.headers.set(k, v);
+    }
   }
   return res;
 }
