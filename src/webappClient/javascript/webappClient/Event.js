098b00881a9fc76bca10dce2c74d7ad4b9374646
Andy Frank <1316916+afrankvt@users.noreply.github.com>
2009-02-25 17:45:00 -0500

    webappClient: Event

---
+++ src/webappClient/javascript/webappClient/Event.js
@@ -0,0 +1,27 @@
+//
+// Copyright (c) 2009, Brian Frank and Andy Frank
+// Licensed under the Academic Free License version 3.0
+//
+// History:
+//   24 Feb 09  Andy Frank  Creation
+//
+
+sys_Type.addType("webappClient::Event");
+var webappClient_Event = sys_Obj.extend(
+{
+  $ctor: function() {},
+  type: function() { return sys_Type.find("webappClient::Event"); },
+
+  target: function()
+  {
+    return webappClient_Elem.make(this.event.target);
+  },
+
+});
+
+webappClient_Event.make = function(event)
+{
+  var wrap = new webappClient_Event();
+  if (event != null) wrap.event = event;
+  return wrap;
+}
