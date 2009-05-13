56b2d123acd605ab159074e290b0895eddfc7dc3
Brian Frank <58821032+briansfrank@users.noreply.github.com>
2009-05-13 17:27:32 -0400

    fanx.sys.Interop utility methods

---
+++ src/sys/java/fanx/interop/Interop.java
@@ -0,0 +1,145 @@
+//
+// Copyright (c) 2009, Brian Frank and Andy Frank
+// Licensed under the Academic Free License version 3.0
+//
+// History:
+//   13 May 09  Brian Frank  Creation
+//
+package fanx.interop;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Iterator;
+import fan.sys.*;
+
+/**
+ * Interop defines for converting between Fan and Java for common types.
+ */
+public class Interop
+{
+
+//////////////////////////////////////////////////////////////////////////
+// IO
+//////////////////////////////////////////////////////////////////////////
+
+  /**
+   * Convert from java.io.InputStream to sys::InStream
+   * with default buffer size of 4096.
+   */
+  public static InStream toFan(InputStream in)
+  {
+    return SysInStream.make(in, FanInt.Chunk);
+  }
+
+  /**
+   * Convert from java.io.InputStream to sys::InStream
+   * with the given buffer size.
+   */
+  public static InStream toFan(InputStream in, long bufSize)
+  {
+    return SysInStream.make(in, bufSize);
+  }
+
+  /**
+   * Convert from java.io.OutputStream to sys::OutStream
+   * with default buffer size of 4096.
+   */
+  public static OutStream toFan(OutputStream out)
+  {
+    return SysOutStream.make(out, FanInt.Chunk);
+  }
+
+  /**
+   * Convert from java.io.OutputStream to sys::OutStream
+   * with the given buffer size.
+   */
+  public static OutStream toFan(OutputStream out, long bufSize)
+  {
+    return SysOutStream.make(out, bufSize);
+  }
+
+  /**
+   * Convert from sys::InStream to java.io.InputStream.
+   */
+  public static InputStream toJava(InStream in)
+  {
+    return SysInStream.java(in);
+  }
+
+  /**
+   * Convert from sys::OutStream to java.io.OutputStream.
+   */
+  public static OutputStream toJava(OutStream out)
+  {
+    return SysOutStream.java(out);
+  }
+
+//////////////////////////////////////////////////////////////////////////
+// Collections
+//////////////////////////////////////////////////////////////////////////
+
+  /**
+   * Convert a java.util.Enumeration to a sys::List with a type of Obj?[].
+   */
+  public static List toFan(Enumeration e)
+  {
+    return toFan(e, Sys.ObjType.toNullable());
+  }
+
+  /**
+   * Convert a java.util.Enumeration to a sys::List of the specified type.
+   */
+  public static List toFan(Enumeration e, Type of)
+  {
+    List list = new List(of);
+    while (e.hasMoreElements()) list.add(e.nextElement());
+    return list;
+  }
+
+  /**
+   * Convert a java.util.Iterator to a sys::List with a type of Obj?[].
+   */
+  public static List toFan(Iterator i)
+  {
+    return toFan(i, Sys.ObjType.toNullable());
+  }
+
+  /**
+   * Convert a java.util.Iterator to a sys::List of the specified type.
+   */
+  public static List toFan(Iterator i, Type of)
+  {
+    List list = new List(of);
+    while (i.hasNext()) list.add(i.next());
+    return list;
+  }
+
+  /**
+   * Convert a java.util.HashMap to a sys::Map with a type of Obj:Obj?.
+   */
+  public static Map toFan(HashMap map)
+  {
+    return new Map(new MapType(Sys.ObjType, Sys.ObjType.toNullable()), map);
+  }
+
+  /**
+   * Convert a java.util.HashMap to a sys::Map with the specified map type.
+   */
+  public static Map toFan(HashMap map, Type type)
+  {
+    return new Map((MapType)type, map);
+  }
+
+  /**
+   * Convert a sys::Map to a java.util.HashMap.  If the fan
+   ** map is not read/write, then ReadonlyErr is thrown.
+   */
+  public static HashMap toJava(Map map)
+  {
+    return map.toJava();
+  }
+
+
+}
