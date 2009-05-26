2a596fcc36e4233501cf8559e734d0e995f2d7f2
Andy Frank <1316916+afrankvt@users.noreply.github.com>
2009-05-26 13:18:51 -0400

    fwt: Initial support for js

---
+++ src/fwt/javascript/PanePeer.js
@@ -0,0 +1,18 @@
+//
+// Copyright (c) 2009, Brian Frank and Andy Frank
+// Licensed under the Academic Free License version 3.0
+//
+// History:
+//   17 May 09  Andy Frank  Creation
+//
+
+/**
+ *PanePeer.
+ */
+var fwt_PanePeer = sys_Obj.$extend(fwt_WidgetPeer);
+
+fwt_PanePeer.prototype.$ctor = function(self)
+{
+  fwt_WidgetPeer.prototype.$ctor.call(this, self);
+}
+
