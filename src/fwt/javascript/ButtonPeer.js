08d3c3b340a45658c4a310aa7382368a5339d995
Andy Frank <Andy Frank@localhost>
2009-05-26 13:18:51 -0400

    fwt: Initial support for js

---
+++ src/fwt/javascript/ButtonPeer.js
@@ -0,0 +1,18 @@
+//
+// Copyright (c) 2009, Brian Frank and Andy Frank
+// Licensed under the Academic Free License version 3.0
+//
+// History:
+//   8 May 09  Andy Frank  Creation
+//
+
+/**
+ * ButtonPeer.
+ */
+var fwt_ButtonPeer = sys_Obj.$extend(fwt_WidgetPeer);
+
+fwt_ButtonPeer.prototype.$ctor = function(self)
+{
+  fwt_WidgetPeer.prototype.$ctor.call(this, self);
+}
+
