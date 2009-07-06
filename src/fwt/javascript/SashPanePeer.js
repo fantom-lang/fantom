f41800305334460befc355474e74ce38684fb407
Andy Frank <1316916+afrankvt@users.noreply.github.com>
2009-07-06 14:44:35 -0400

    fwt-js: SashPane.weights

--- src/fwt/javascript/SashPanePeer.js
+++ src/fwt/javascript/SashPanePeer.js
@@ -19,6 +19,9 @@ fwt_SashPanePeer.prototype.weights = null;
 
 fwt_SashPanePeer.prototype.sync = function(self)
 {
+  if (this.weights != null && this.weights.length != self.kids.length)
+    throw new sys_ArgErr.make("weights.size != kids.length");
+
   if (self.orientation == fwt_Orientation.horizontal)
     this.doHoriz(self);
   else
@@ -30,6 +33,7 @@ fwt_SashPanePeer.prototype.doHoriz = function(self)
 {
   var w = this.size.w;
   var h = this.size.h;
+  var wt = this.weights;
 
   var dy = 0;
   var dh = Math.floor(h /self.kids.length);
@@ -37,7 +41,10 @@ fwt_SashPanePeer.prototype.doHoriz = function(self)
   for (var i=0; i<self.kids.length; i++)
   {
     var cw = w;
-    var ch = (i<self.kids.length-1) ? dh : h-dy; // give last widget leftovers
+    var ch = wt==null ? dh : Math.floor(h * (wt[i].valueOf() / 100));
+
+    // if last widget, force to fill remaining space
+    if (i == self.kids.length-1) ch = h-dy;
 
     self.kids[i].pos$set(gfx_Point.make(0, dy));
     self.kids[i].size$set(gfx_Size.make(cw, ch));
@@ -50,14 +57,18 @@ fwt_SashPanePeer.prototype.doVert = function(self)
 {
   var w = this.size.w;
   var h = this.size.h;
+  var wt = this.weights
 
   var dx = 0;
   var dw = Math.floor(w / self.kids.length);
 
   for (var i=0; i<self.kids.length; i++)
   {
-    var cw = (i<self.kids.length-1) ? dw : w-dx; // give last widget leftovers
+    var cw = wt==null ? dw : Math.floor(w * (wt[i].valueOf() / 100));
     var ch = h;
+
+    // if last widget, force to fill remaining space
+    if (i == self.kids.length-1) cw = w-dx;
 
     self.kids[i].pos$set(gfx_Point.make(dx, 0));
     self.kids[i].size$set(gfx_Size.make(cw, ch));
