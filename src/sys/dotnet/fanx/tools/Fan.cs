d5187ea13a3ee6f75f007a0cf8b5abcd173a19bf
Brian Frank <Brian Frank@localhost>
2009-07-24 11:49:45 -0400

    repo: docs

--- src/sys/dotnet/fanx/tools/Fan.cs
+++ src/sys/dotnet/fanx/tools/Fan.cs
@@ -153,7 +153,7 @@ namespace Fanx.Tools
     internal static void version(string progName)
     {
       writeLine(progName);
-      writeLine("Copyright (c) 2006-2008, Brian Frank and Andy Frank");
+      writeLine("Copyright (c) 2006-2009, Brian Frank and Andy Frank");
       writeLine("Licensed under the Academic Free License version 3.0");
       writeLine("");
       writeLine(".NET Runtime:");
