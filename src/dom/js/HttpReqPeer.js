6d2f6ded6e27ea6f980a42a7091ba536d6a26187
Andy Frank <Andy Frank@localhost>
2009-08-25 10:47:24 -0400

    dom: Fix HttpReq sync requests

--- src/dom/js/HttpReqPeer.js
+++ src/dom/js/HttpReqPeer.js
@@ -14,29 +14,14 @@ fan.dom.HttpReqPeer.prototype.$ctor = function(self) {}
 
 fan.dom.HttpReqPeer.prototype.send = function(self, content, func)
 {
-  var req = new XMLHttpRequest();
-  req.open(self.m_method, self.m_uri.m_str, self.m_async);
+  var xhr = new XMLHttpRequest();
+  xhr.open(self.m_method, self.m_uri.m_str, self.m_async);
   if (self.m_async)
   {
-    req.onreadystatechange = function () {
-      if (req.readyState == 4)
-      {
-        var res = fan.dom.HttpRes.make();
-        res.m_status  = req.status;
-        res.m_content = req.responseText;
-
-        var all = req.getAllResponseHeaders().split("\n");
-        for (var i=0; i<all.length; i++)
-        {
-          if (all[i].length == 0) continue;
-          var j = all[i].indexOf(":");
-          var k = fan.sys.Str.trim(all[i].substr(0, j));
-          var v = fan.sys.Str.trim(all[i].substr(j+1));
-          res.m_headers.set(k, v);
-        }
-
-        func(res);
-      }
+    xhr.onreadystatechange = function ()
+    {
+      if (xhr.readyState == 4)
+        func(fan.dom.HttpReqPeer.makeRes(xhr));
     }
   }
   var ct = false;
@@ -44,11 +29,30 @@ fan.dom.HttpReqPeer.prototype.send = function(self, content, func)
   for (var i=0; i<k.length; i++)
   {
     if (fan.sys.Str.lower(k[i]) == "content-type") ct = true;
-    req.setRequestHeader(k[i], self.m_headers.get(k[i]));
+    xhr.setRequestHeader(k[i], self.m_headers.get(k[i]));
   }
-  if (!ct) req.setRequestHeader("Content-Type", "text/plain");
-  req.send(content);
-  if (!self.m_async) func(fan.dom.HttpRes.make(req));
+  if (!ct) xhr.setRequestHeader("Content-Type", "text/plain");
+  xhr.send(content);
+  if (!self.m_async) func(fan.dom.HttpReqPeer.makeRes(xhr));
+}
+
+fan.dom.HttpReqPeer.makeRes = function(xhr)
+{
+  var res = fan.dom.HttpRes.make();
+  res.m_status  = xhr.status;
+  res.m_content = xhr.responseText;
+
+  var all = xhr.getAllResponseHeaders().split("\n");
+  for (var i=0; i<all.length; i++)
+  {
+    if (all[i].length == 0) continue;
+    var j = all[i].indexOf(":");
+    var k = fan.sys.Str.trim(all[i].substr(0, j));
+    var v = fan.sys.Str.trim(all[i].substr(j+1));
+    res.m_headers.set(k, v);
+  }
+
+  return res;
 }
 
 fan.dom.HttpReqPeer.prototype.sendForm = function(self, form, func)
