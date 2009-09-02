fb90dd22b4f2b34c3ab81d9f0c6a95a4795e43dc
Andy Frank <Andy Frank@localhost>
2009-09-01 23:36:39 -0400

    fwt-js: Support for ButtonMode.toggle for Button

--- src/fwt/js/ButtonPeer.js
+++ src/fwt/js/ButtonPeer.js
@@ -28,9 +28,13 @@ fan.fwt.ButtonPeer.prototype.text = function(self) { return this.m_text; }
 fan.fwt.ButtonPeer.prototype.text$ = function(self, val) { this.m_text = val; }
 fan.fwt.ButtonPeer.prototype.m_text = "";
 
+fan.fwt.ButtonPeer.prototype.m_pressed = false;
+
 fan.fwt.ButtonPeer.prototype.create = function(parentElem, self)
 {
-  if (self.m_mode == fan.fwt.ButtonMode.m_push) return this.makePush(parentElem, self);
+  if (self.m_mode == fan.fwt.ButtonMode.m_push || self.m_mode == fan.fwt.ButtonMode.m_toggle)
+    return this.makePush(parentElem, self);
+
   if (self.m_mode == fan.fwt.ButtonMode.m_check) return this.makeCheck(parentElem, self);
 }
 
@@ -45,25 +49,22 @@ fan.fwt.ButtonPeer.prototype.makePush = function(parentElem, self)
   var inner = document.createElement("div");
   with (inner.style)
   {
-    //font      = "bold 10pt Arial";
-    padding      = "2px 4px";
-    textAlign    = "center";
-    borderTop    = "1px solid #fff";
-    borderLeft   = "1px solid #fff";
-    borderRight  = "1px solid #cacaca";
-    borderBottom = "1px solid #cacaca";
-    cursor       = "default";
-    whiteSpace   = "nowrap";
+    //font       = "bold 10pt Arial";
+    textAlign  = "center";
+    cursor     = "default";
+    whiteSpace = "nowrap";
     //textShadow = "0 1px 1px #fff";
-    backgroundColor = "#eee";
-
-    // IE workaround
-    try { backgroundImage = "-webkit-gradient(linear, 0% 0%, 0% 100%, from(#f6f6f6), to(#dadada))"; } catch (err) {} // ignore
+    // use repaint for styles that change b/w pressed/unpressed
   }
 
+  var $this = this;
   outer.onclick = function(event)
   {
     if (!self.enabled()) return;
+
+    // toggle selected if toggle mode
+    if (self.m_mode == fan.fwt.ButtonMode.m_toggle)
+      $this.m_selected = !$this.m_selected;
 
     var evt = new fan.fwt.Event();
     evt.m_id = fan.fwt.EventId.m_action;
@@ -76,35 +77,16 @@ fan.fwt.ButtonPeer.prototype.makePush = function(parentElem, self)
   outer.onmousedown = function(event)
   {
     if (!self.enabled()) return;
-    with (inner.style)
-    {
-      padding = "3px 3px 1px 5px";
-      borderTop    = "1px solid #a8a8a8";
-      borderLeft   = "1px solid #a8a8a8";
-      borderRight  = "none";
-      borderBottom = "none";
-      backgroundColor = "#c0c0c0";
-
-      // IE workaround
-      try { backgroundImage = "-webkit-gradient(linear, 0% 0%, 0% 100%, from(#c0c0c0), to(#dadada))"; } catch (err) {} // ignore
-    }
+    $this.m_pressed = true;
+    $this.repaint(self);
   }
 
   var up = function(event)
   {
     if (!self.enabled()) return;
-    with (inner.style)
-    {
-      padding = "2px 4px";
-      borderTop    = "1px solid #fff";
-      borderLeft   = "1px solid #fff";
-      borderRight  = "1px solid #cacaca";
-      borderBottom = "1px solid #cacaca";
-      backgroundColor = "#eee";
-
-      // IE workaround
-      try { backgroundImage = "-webkit-gradient(linear, 0% 0%, 0% 100%, from(#f6f6f6), to(#dadada))"; } catch (err) {} // ignore
-    }
+    if (self.m_mode == fan.fwt.ButtonMode.m_toggle && $this.m_selected) return;
+    $this.m_pressed = false;
+    $this.repaint(self);
   }
   outer.onmouseout = function(event) { up(event); }
   outer.onmouseup = function(event) { up(event); }
@@ -142,12 +124,55 @@ fan.fwt.ButtonPeer.prototype.makeCheck = function(parentElem, self)
   return div;
 }
 
+fan.fwt.ButtonPeer.prototype.repaint = function(self)
+{
+  if (self.m_mode == fan.fwt.ButtonMode.m_push ||
+      self.m_mode == fan.fwt.ButtonMode.m_toggle)
+  {
+    var outer = this.elem;
+    var inner = outer.firstChild;
+    var pressed = this.m_pressed || this.m_selected;
+
+    if (pressed)
+    {
+      with (inner.style)
+      {
+        padding = "3px 3px 1px 5px";
+        borderTop    = "1px solid #a8a8a8";
+        borderLeft   = "1px solid #a8a8a8";
+        borderRight  = "none";
+        borderBottom = "none";
+        backgroundColor = "#c0c0c0";
+
+        // IE workaround
+        try { backgroundImage = "-webkit-gradient(linear, 0% 0%, 0% 100%, from(#c0c0c0), to(#dadada))"; } catch (err) {} // ignore
+      }
+    }
+    else
+    {
+      with (inner.style)
+      {
+        padding = "2px 4px";
+        borderTop    = "1px solid #fff";
+        borderLeft   = "1px solid #fff";
+        borderRight  = "1px solid #cacaca";
+        borderBottom = "1px solid #cacaca";
+        backgroundColor = "#eee";
+
+        // IE workaround
+        try { backgroundImage = "-webkit-gradient(linear, 0% 0%, 0% 100%, from(#f6f6f6), to(#dadada))"; } catch (err) {} // ignore
+      }
+    }
+  }
+}
+
 fan.fwt.ButtonPeer.prototype.sync = function(self)
 {
   var w = this.m_size.m_w;
   var h = this.m_size.m_h;
 
-  if (self.m_mode == fan.fwt.ButtonMode.m_push)
+  if (self.m_mode == fan.fwt.ButtonMode.m_push ||
+      self.m_mode == fan.fwt.ButtonMode.m_toggle)
   {
     var div = this.elem.firstChild;
 
@@ -175,12 +200,13 @@ fan.fwt.ButtonPeer.prototype.sync = function(self)
 
     // set state
     var check = this.elem.firstChild;
-    check.checked = self.m_selected;
+    check.checked = this.m_selected;
 
     // set text
     while (div.childNodes.length > 1) div.removeChild(div.lastChild);
     div.appendChild(document.createTextNode(this.m_text));
   }
 
+  this.repaint(self);
   fan.fwt.WidgetPeer.prototype.sync.call(this, self, w, h);
 }
