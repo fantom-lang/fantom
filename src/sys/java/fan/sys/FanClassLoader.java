636998587ec812e251f70cfaa26a1be626b1f566
Brian Frank <58821032+briansfrank@users.noreply.github.com>
2009-11-17 09:34:09 -0500

    error message redirects to docs if SWT fails to load (try again)

--- src/sys/java/fan/sys/FanClassLoader.java
+++ src/sys/java/fan/sys/FanClassLoader.java
@@ -111,99 +111,98 @@ public class FanClassLoader
   protected Class findClass(String name)
     throws ClassNotFoundException
   {
-    // first check if the classfile in my pending queue
-    Box pending = null;
-    synchronized(pendingClasses)
-    {
-      pending = (Box)pendingClasses.get(name);
-      if (pending != null) pendingClasses.remove(name);
-    }
-
-    // if it was pending, then use that to define new class
-    if (pending != null)
-    {
-// if (name.endsWith("")) dumpToFile(name, pending);
-      Class cls = defineClass(name, pending.buf, 0, pending.len, codeSource);
-      return cls;
-    }
-
-    // anything starting with "fan." maps to a Fan Type (or native peer code)
-    if (name.startsWith("fan."))
-    {
-      // fan.{pod}.{type}
-      int dot = name.indexOf('.', 4);
-      String podName  = name.substring(4, dot);
-      String typeName = name.substring(dot+1);
-      Pod pod = Pod.doFind(podName, true, null, null);
-
-      // see if we can find a precompiled class
-      if (pod.fpod.store != null)
-      {
-        try
-        {
-          String path = "fan/" + podName + "/" + typeName + ".class";
-          Box precompiled = pod.fpod.store.readToBox(path);
-          if (precompiled != null)
-          {
-//dumpToFile(name, precompiled);
-            Class cls = defineClass(name, precompiled.buf, 0, precompiled.len, codeSource);
-
-            // if the precompiled class is a fan type, then we need
-            // to finish the emit process since we skipped the normal
-            // code path thru Type.emit() for fcode to bytecode generation
-            ClassType type = (ClassType)pod.findType(typeName, false);
-            if (type != null) type.precompiled(cls);
-
-            // if the class is a precompiled Pod type
-            else if (typeName.equals("$Pod")) pod.precompiled(cls);
-
-            return cls;
-          }
-        }
-        catch (Exception e)
-        {
-          e.printStackTrace();
-        }
-      }
-
-      // ensure pod is emitted with our constant pool before
-      // loading any classes inside of it (if this was the
-      // actual class to load, then we are done)
-      pod.emit();
-      if (typeName.equals("$Pod")) return pod.emit();
-
-      // if the type name ends with $ then this is a mixin body
-      // class being used before we have loaded the mixin interface,
-      // so load them both, then we should it registered by name;
-      // same goes for $Val for Err value types
-      if (typeName.endsWith("$") || typeName.endsWith("$Val"))
-      {
-        int strip = typeName.endsWith("$") ? 1 : 4;
-        ClassType t = (ClassType)pod.findType(typeName.substring(0, typeName.length()-strip), true);
-        Class cls = t.emit();
-        return loadClass(name);
-      }
-
-      // if there wasn't a precompiled class, then this must
-      // be a normal fcode type which we need to emit
-      ClassType t = (ClassType)pod.findType(typeName, true);
-      return t.emit();
-    }
-
-    // fallback to default URLClassLoader loader
-    // implementation which searches my ext jars
     try
     {
+      // first check if the classfile in my pending queue
+      Box pending = null;
+      synchronized(pendingClasses)
+      {
+        pending = (Box)pendingClasses.get(name);
+        if (pending != null) pendingClasses.remove(name);
+      }
+
+      // if it was pending, then use that to define new class
+      if (pending != null)
+      {
+  // if (name.endsWith("")) dumpToFile(name, pending);
+        Class cls = defineClass(name, pending.buf, 0, pending.len, codeSource);
+        return cls;
+      }
+
+      // anything starting with "fan." maps to a Fan Type (or native peer code)
+      if (name.startsWith("fan."))
+      {
+        // fan.{pod}.{type}
+        int dot = name.indexOf('.', 4);
+        String podName  = name.substring(4, dot);
+        String typeName = name.substring(dot+1);
+        Pod pod = Pod.doFind(podName, true, null, null);
+
+        // see if we can find a precompiled class
+        if (pod.fpod.store != null)
+        {
+          try
+          {
+            String path = "fan/" + podName + "/" + typeName + ".class";
+            Box precompiled = pod.fpod.store.readToBox(path);
+            if (precompiled != null)
+            {
+  //dumpToFile(name, precompiled);
+              Class cls = defineClass(name, precompiled.buf, 0, precompiled.len, codeSource);
+
+              // if the precompiled class is a fan type, then we need
+              // to finish the emit process since we skipped the normal
+              // code path thru Type.emit() for fcode to bytecode generation
+              ClassType type = (ClassType)pod.findType(typeName, false);
+              if (type != null) type.precompiled(cls);
+
+              // if the class is a precompiled Pod type
+              else if (typeName.equals("$Pod")) pod.precompiled(cls);
+
+              return cls;
+            }
+          }
+          catch (Exception e)
+          {
+            e.printStackTrace();
+          }
+        }
+
+        // ensure pod is emitted with our constant pool before
+        // loading any classes inside of it (if this was the
+        // actual class to load, then we are done)
+        pod.emit();
+        if (typeName.equals("$Pod")) return pod.emit();
+
+        // if the type name ends with $ then this is a mixin body
+        // class being used before we have loaded the mixin interface,
+        // so load them both, then we should it registered by name;
+        // same goes for $Val for Err value types
+        if (typeName.endsWith("$") || typeName.endsWith("$Val"))
+        {
+          int strip = typeName.endsWith("$") ? 1 : 4;
+          ClassType t = (ClassType)pod.findType(typeName.substring(0, typeName.length()-strip), true);
+          Class cls = t.emit();
+          return loadClass(name);
+        }
+
+        // if there wasn't a precompiled class, then this must
+        // be a normal fcode type which we need to emit
+        ClassType t = (ClassType)pod.findType(typeName, true);
+        return t.emit();
+      }
+
+      // fallback to default URLClassLoader loader
+      // implementation which searches my ext jars
       return super.findClass(name);
     }
-    catch (NoClassDefFoundError e)   { checkLoadFailure(name); throw e; }
-    catch (ClassNotFoundException e) { checkLoadFailure(name); throw e; }
-  }
-
-  private static void checkLoadFailure(String name)
-  {
-    if (name.contains("eclipse.swt"))
-      System.out.println("ERROR: cannot load SWT library - see `http://fantom.org/doc/docTools/Setup.html#swt`");
+    catch (NoClassDefFoundError e)
+    {
+      String s = e.toString();
+      if (s.contains("eclipse") && s.contains("swt"))
+        System.out.println("ERROR: cannot load SWT library - see `http://fantom.org/doc/docTools/Setup.html#swt`");
+      throw e;
+    }
   }
 
 //////////////////////////////////////////////////////////////////////////
