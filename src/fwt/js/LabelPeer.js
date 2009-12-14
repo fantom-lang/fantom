43689c23d74939345f20bdace53e9264ee01d413
Andy Frank <1316916+afrankvt@users.noreply.github.com>
2009-12-14 16:47:00 -0500

    fwt-js: Fix NPE when setting Layout.image to null

--- src/fwt/js/LabelPeer.js
+++ src/fwt/js/LabelPeer.js
@@ -57,7 +57,8 @@ fan.fwt.LabelPeer.prototype.image  = function(self) { return this.m_image; }
 fan.fwt.LabelPeer.prototype.image$ = function(self, val)
 {
   this.m_image = val;
-  fan.fwt.FwtEnvPeer.loadImage(val, self)
+  if (val != null)
+    fan.fwt.FwtEnvPeer.loadImage(val, self)
   this.needRebuild = true;
 }
 
