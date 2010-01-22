42b3836864df1b791567cd5cdbec1756c852d5c8
Andy Frank <afrankvt@gmail.com>
2010-01-22 14:42:31 -0500

    Locale stubs for tests; Bool.toLocale

--- src/sys/js/fan/Locale.js
+++ src/sys/js/fan/Locale.js
@@ -11,7 +11,28 @@
  */
 fan.sys.Locale = fan.sys.Obj.$extend(fan.sys.Obj);
 
-fan.sys.Locale.prototype.$ctor = function() {}
-fan.sys.Locale.prototype.$typeof = function() { return fan.sys.Locale.$type; }
+//////////////////////////////////////////////////////////////////////////
+// Constructors
+//////////////////////////////////////////////////////////////////////////
 
-fan.sys.Locale.cur = function() { return null; }
+fan.sys.Locale.make = function()
+{
+  var self = new fan.sys.Locale();
+  return self;
+}
+
+fan.sys.Locale.prototype.$ctor = function() {}
+
+//////////////////////////////////////////////////////////////////////////
+// Methods
+//////////////////////////////////////////////////////////////////////////
+
+fan.sys.Locale.prototype.$typeof = function() { return fan.sys.Locale.$type; }
+fan.sys.Locale.cur = function()
+{
+  if (fan.sys.Locale.$cur == null) fan.sys.Locale.$cur = fan.sys.Locale.make();
+  return fan.sys.Locale.$cur;
+}
+fan.sys.Locale.fromStr = function(str) { return fan.sys.Locale.make(); }
+fan.sys.Locale.prototype.use = function(f) { f.call(this); }
+fan.sys.Locale.prototype.get = function(pod, key, def) { return def; }
