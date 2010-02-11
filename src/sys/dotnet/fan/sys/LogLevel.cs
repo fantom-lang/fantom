e7a5287636fb7eca54ae72539fbaf2e174f03b24
Andy Frank <afrankvt@gmail.com>
2010-02-11 09:51:54 -0500

    Make LogLevel.vals immutable for C#

--- src/sys/dotnet/fan/sys/LogLevel.cs
+++ src/sys/dotnet/fan/sys/LogLevel.cs
@@ -26,7 +26,7 @@ namespace Fan.Sys
 
     static readonly LogLevel[] array = { m_debug, m_info, m_warn, m_err, m_silent };
 
-    public static readonly List m_vals = new List(Sys.LogLevelType, array).ro();
+    public static readonly List m_vals = (List)new List(Sys.LogLevelType, array).toImmutable();
 
   //////////////////////////////////////////////////////////////////////////
   // Implementation
