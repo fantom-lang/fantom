bc5a2d09d9e39997b7e69dfa87fad81a84569031
Andy Frank <1316916+afrankvt@users.noreply.github.com>
2010-05-04 10:00:53 -0400

    Pass testSys::LocaleTest in js

--- src/sys/js/fan/Locale.js
+++ src/sys/js/fan/Locale.js
@@ -12,27 +12,93 @@
 fan.sys.Locale = fan.sys.Obj.$extend(fan.sys.Obj);
 
 //////////////////////////////////////////////////////////////////////////
-// Constructors
+// Construction
 //////////////////////////////////////////////////////////////////////////
 
-fan.sys.Locale.make = function()
+fan.sys.Locale.fromStr = function(s, checked)
 {
-  var self = new fan.sys.Locale();
-  return self;
+  if (checked === undefined) checked = true;
+
+  var len = s.length;
+  try
+  {
+    if (len == 2)
+    {
+      if (fan.sys.Str.isLower(s))
+        return new fan.sys.Locale(s, s, null);
+    }
+
+    if (len == 5)
+    {
+      var lang = s.substring(0, 2);
+      var country = s.substring(3, 5);
+      if (fan.sys.Str.isLower(lang) && fan.sys.Str.isUpper(country) && s.charAt(2) == '-')
+        return new fan.sys.Locale(s, lang, country);
+    }
+  }
+  catch (err) {}
+  if (!checked) return null;
+  throw fan.sys.ParseErr.make("Locale", s);
 }
 
-fan.sys.Locale.prototype.$ctor = function() {}
+fan.sys.Locale.prototype.$ctor = function(str, lang, country)
+{
+  this.m_str       = str;
+  this.m_lang      = lang;
+  this.m_country   = country;
+  this.m_strProps  = fan.sys.Uri.fromStr("locale/" + str + ".props");
+  this.m_langProps = fan.sys.Uri.fromStr("locale/" + lang + ".props");
+}
+
+//////////////////////////////////////////////////////////////////////////
+// Thread
+//////////////////////////////////////////////////////////////////////////
+
+fan.sys.Locale.cur = function()
+{
+  if (fan.sys.Locale.$cur == null) fan.sys.Locale.$cur = fan.sys.Locale.fromStr("en");
+  return fan.sys.Locale.$cur;
+}
+
+fan.sys.Locale.setCur = function(locale)
+{
+  if (locale == null) throw fan.sys.NullErr.make();
+  fan.sys.Locale.$cur = locale;
+}
+
+fan.sys.Locale.prototype.use = function(func)
+{
+  var old = fan.sys.Locale.cur();
+  try
+  {
+    fan.sys.Locale.setCur(this);
+    func.call(this);
+  }
+  finally
+  {
+    fan.sys.Locale.setCur(old);
+  }
+  return this;
+}
 
 //////////////////////////////////////////////////////////////////////////
 // Methods
 //////////////////////////////////////////////////////////////////////////
 
+fan.sys.Locale.prototype.lang = function() { return this.m_lang; }
+
+fan.sys.Locale.prototype.country = function() { return this.m_country; }
+
 fan.sys.Locale.prototype.$typeof = function() { return fan.sys.Locale.$type; }
-fan.sys.Locale.cur = function()
+
+fan.sys.Locale.prototype.hash = function() { return fan.sys.Str.hash(this.m_str); }
+
+fan.sys.Locale.prototype.equals = function(obj)
 {
-  if (fan.sys.Locale.$cur == null) fan.sys.Locale.$cur = fan.sys.Locale.make();
-  return fan.sys.Locale.$cur;
+  if (obj instanceof fan.sys.Locale)
+    return obj.m_str == this.m_str;
+  return false;
 }
-fan.sys.Locale.fromStr = function(str) { return fan.sys.Locale.make(); }
-fan.sys.Locale.prototype.use = function(f) { f.call(this); }
-fan.sys.Locale.prototype.get = function(pod, key, def) { return def; }
+
+fan.sys.Locale.prototype.toStr = function() { return this.m_str; }
+
