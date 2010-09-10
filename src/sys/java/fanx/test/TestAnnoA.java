6ddd1f62ba7010b0197760f3e7dd0964472f79fb
Brian Frank <58821032+briansfrank@users.noreply.github.com>
2010-09-10 07:14:36 -0400

    Annotations: stub Java FFI to support marker annotations as Fantom facets (#668)

---
+++ src/sys/java/fanx/test/TestAnnoA.java
@@ -0,0 +1,10 @@
+//
+// Copyright (c) 2010, Brian Frank and Andy Frank
+// Licensed under the Academic Free License version 3.0
+//
+// History:
+//   10 Sep 10  Brian Frank  Creation
+//
+package fanx.test;
+
+public @interface TestAnnoA {}
