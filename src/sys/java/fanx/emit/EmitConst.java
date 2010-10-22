af33a227702361b97946d97411b12987b932f377
Brian Frank <58821032+briansfrank@users.noreply.github.com>
2010-10-22 10:08:08 -0400

    Java emitter: add synthetic flag to mixin routers for step debugging

--- src/sys/java/fanx/emit/EmitConst.java
+++ src/sys/java/fanx/emit/EmitConst.java
@@ -28,16 +28,19 @@ public interface EmitConst
 // Access Flags
 //////////////////////////////////////////////////////////////////////////
 
-  public static final int PUBLIC    = 0x0001;
-  public static final int PRIVATE   = 0x0002;
-  public static final int PROTECTED = 0x0004;
-  public static final int STATIC    = 0x0008;
-  public static final int FINAL     = 0x0010;
-  public static final int SUPER     = 0x0020;
-  public static final int VOLATILE  = 0x0040;
-  public static final int INTERFACE = 0x0200;
-  public static final int ABSTRACT  = 0x0400;
-  public static final int TRANSIENT = 0x0800;
+  public static final int PUBLIC     = 0x0001;
+  public static final int PRIVATE    = 0x0002;
+  public static final int PROTECTED  = 0x0004;
+  public static final int STATIC     = 0x0008;
+  public static final int FINAL      = 0x0010;
+  public static final int SUPER      = 0x0020;
+  public static final int VOLATILE   = 0x0040;
+  public static final int INTERFACE  = 0x0200;
+  public static final int ABSTRACT   = 0x0400;
+  public static final int TRANSIENT  = 0x0800;
+  public static final int SYNTHETIC  = 0x1000;
+  public static final int ANNOTATION = 0x2000;
+  public static final int ENUM       = 0x4000;
 
 //////////////////////////////////////////////////////////////////////////
 // Constant Types
