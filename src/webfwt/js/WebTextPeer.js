323712d2c4bf32c7f1941e849d3f1ee8e3948500
Andy Frank <1316916+afrankvt@users.noreply.github.com>
2012-09-20 18:14:08 -0400

    fwt: Text.fg/bg; webfwt: WebText.insets/innerShadow/dropShadow/border/placeHolderColor

--- src/webfwt/js/WebTextPeer.js
+++ src/webfwt/js/WebTextPeer.js
@@ -15,10 +15,62 @@ fan.webfwt.WebTextPeer.prototype.$ctor = function(self)
   fan.fwt.TextPeer.prototype.$ctor.call(this, self);
 }
 
+fan.webfwt.WebTextPeer.counter = 0;
+fan.webfwt.WebTextPeer.prototype.subCreate = function(self, control)
+{
+  // placeHolder
+  if (self.m_placeHolder) control.placeholder = self.m_placeHolder;
+
+  // shadow/insets/border
+  this.setShadow(self, control);
+  if (self.m_insets) control.style.padding = fan.fwt.WidgetPeer.insetsToCss(self.m_insets);
+  if (self.m_textBorder) fan.fwt.WidgetPeer.setBorder(control, self.m_textBorder);
+
+  var phColor = self.placeHolderColor();
+  if (phColor)
+  {
+    var cls = this.subClass = "_webTextCls_" + (fan.webfwt.WebTextPeer.counter++);
+    fan.fwt.WidgetPeer.addCss(
+      "input." + cls + "::-webkit-input-placeholder { color:" + phColor.toCss() + "}" +
+      "input." + cls + ":-moz-placeholder { color:" + phColor.toCss() + "}" +
+      "input." + cls + ":-ms-input-placeholder { color:" + phColor.toCss() + "}");
+    fan.fwt.WidgetPeer.addClassName(control, cls);
+  }
+}
+
+fan.webfwt.WebTextPeer.prototype.setShadow = function(self, control)
+{
+  var shadow = "";
+  if (self.m_dropShadow != null)
+    shadow += self.m_dropShadow.toCss();
+
+  if (self.m_innerShadow != null)
+  {
+    if (shadow.length > 0) shadow += ",";
+    shadow += "inset " + self.m_innerShadow.toCss();
+  }
+
+  control.style.webkitBoxShadow = shadow;
+  control.style.MozBoxShadow = shadow;
+  control.style.boxShadow = shadow;
+}
+
+fan.webfwt.WebTextPeer.prototype.subSync = function(self, control)
+{
+  var w = this.m_size.m_w;
+  var h = this.m_size.m_h;
+  var d = self.m_dropShadow;
+  if (d != null) h -= d.m_offset.m_y + d.m_blur + d.m_spread;
+
+  control.style.height = h + "px";
+}
+
 // backdoor hook to override style
 fan.webfwt.WebTextPeer.prototype.$style = function(self) { return self.m_style; }
 fan.webfwt.WebTextPeer.prototype.$disabledStyle = function(self) { return self.m_disabledStyle; }
-
-// backdoor hook to add placeholder text
-fan.webfwt.WebTextPeer.prototype.$placeHolder = function(self) { return self.m_placeHolder; }
-
+fan.webfwt.WebTextPeer.prototype.$cssClass = function(readonly)
+{
+  var cls = fan.fwt.TextPeer.prototype.$cssClass.call(this, readonly);
+  if (this.subClass) cls += " " + this.subClass;
+  return cls;
+}
