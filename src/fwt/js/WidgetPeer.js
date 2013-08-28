7c5f8d42a61aaa84b8b34d6db9ec26a4c331a640
Andy Frank <1316916+afrankvt@users.noreply.github.com>
2013-08-28 16:15:39 -0400

    fwt-js: tweak scroll wheel deltas for browsers

--- src/fwt/js/WidgetPeer.js
+++ src/fwt/js/WidgetPeer.js
@@ -422,20 +422,22 @@ fan.fwt.WidgetPeer.toWheelDelta = function(e)
     wy = -e.wheelDeltaY;
 
     // Safari
-    if (wx % 120 == 0) wx = wx / 40;
-    if (wy % 120 == 0) wy = wy / 40;
+    if (fan.fwt.DesktopPeer.$isMac)
+    {
+      if (wx % 120 == 0) wx = wx / 40;
+      if (wy % 120 == 0) wy = wy / 40;
+    }
   }
   else if (e.wheelDelta != null)
   {
     // IE
     wy = -e.wheelDelta;
-    if (wy % 120 == 0) wy = wy / 40;
   }
   else if (e.detail != null)
   {
     // Firefox
-    wx = e.axis == 1 ? (e.detail * 3) : 0;
-    wy = e.axis == 2 ? (e.detail * 3) : 0;
+    wx = e.axis == 1 ? (e.detail * 40) : 0;
+    wy = e.axis == 2 ? (e.detail * 40) : 0;
   }
 
   // make sure we have ints and return
