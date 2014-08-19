31e40eb712c1020d1cda9b40d542a0e846cba3da
Matthew Giannini <matthew@skyfoundry.com>
2014-08-19 14:27:50 -0400

    sys: fix Env.js to have index with type [Str:Str[]]

--- src/sys/js/fan/Env.js
+++ src/sys/js/fan/Env.js
@@ -25,7 +25,7 @@ fan.sys.Env.prototype.$ctor = function()
 {
   this.m_args = fan.sys.List.make(fan.sys.Str.$type).toImmutable();
 
-  this.m_index = fan.sys.Map.make(fan.sys.Str.$type, fan.sys.List.$type);
+  this.m_index = fan.sys.Map.make(fan.sys.Str.$type, new fan.sys.ListType(fan.sys.Str.$type));
   this.m_index = this.m_index.toImmutable();
 
   this.m_vars = fan.sys.Map.make(fan.sys.Str.$type, fan.sys.Str.$type)
@@ -160,5 +160,3 @@ fan.sys.Env.prototype.$props = function(key)
   }
   return map;
 }
-
-
