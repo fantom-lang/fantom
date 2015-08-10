e6353bc955e4e14dba99e0f17dd72fc294cb8065
Matthew Giannini <1274158+mgiannini@users.noreply.github.com>
2015-08-10 13:45:49 -0400

    fansc: Rename FanSc to fansc and add new fansc binary

---
+++ src/fansc/FanSc/FanSc.cs
@@ -0,0 +1,137 @@
+﻿//
+// Copyright (c) 2015, Brian Frank and Andy Frank
+// Licensed under the Academic Free License version 3.0
+//
+// History:
+//   31 Jul 15  Matthew Giannini    Creation
+//
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration.Install;
+using System.IO;
+using System.ServiceProcess;
+
+namespace FanSc
+{
+    public class FanSc
+    {
+        private string[] rawArgs;
+
+        public FanSc(string[] rawArgs)
+        {
+            this.rawArgs = rawArgs;
+        }
+
+        public void Run()
+        {
+            string cmd = rawArgs[0];
+            switch (cmd.ToLower())
+            {
+                case "install":
+                    DoInstall();
+                    break;
+                case "uninstall":
+                    DoUninstall();
+                    break;
+                default:
+                    ServiceBase.Run(new FanService(rawArgs));
+                    break;
+            }
+        }
+
+        private void DoInstall()
+        {
+            List <string> installerArgs = new List<string>();
+            installerArgs.Add("/LogToConsole=true");
+            installerArgs.Add("/ServiceName=" + ServiceName);
+            installerArgs.Add("/FanLauncher=" + FanLauncher);
+            installerArgs.Add("/LauncherArgs=" + LauncherArgs);
+
+            IDictionary state = new Hashtable();
+            string exe = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            AssemblyInstaller installer = new AssemblyInstaller(exe, installerArgs.ToArray());
+            installer.UseNewContext = true;
+            installer.Install(state);
+            installer.Commit(state);
+        }
+
+        private void DoUninstall()
+        {
+            List<string> installerArgs = new List<string>();
+            installerArgs.Add("/LogToConsole=true");
+            installerArgs.Add("/ServiceName=" + ServiceName);
+
+            IDictionary state = new Hashtable();
+            string exe = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            AssemblyInstaller installer = new AssemblyInstaller(exe, installerArgs.ToArray());
+            installer.UseNewContext = true;
+            installer.Uninstall(state);
+        }
+
+        public string ServiceName
+        {
+            get
+            {
+                if (rawArgs.Length < 2) Usage("ServiceName required");
+                return rawArgs[1];
+            }
+        }
+
+        public string FanLauncher
+        {
+            get
+            {
+                if (rawArgs.Length < 3) Usage("FanLauncher argument required");
+                string launcher = Path.GetFullPath(rawArgs[2].Trim(' ', '\'', '"'));
+                if (!File.Exists(launcher))
+                {
+                    Console.Error.WriteLine("Fantom launcher '" + launcher + "' does not exist");
+                    Environment.Exit(2);
+                }
+                launcher = String.Format("\"{0}\"", launcher);
+                return launcher;
+            }
+        }
+
+        public string LauncherArgs
+        {
+            get
+            {
+                // super-naive approach right now
+                string args = "";
+                for (int i=3; i < rawArgs.Length; ++i)
+                {
+                    args += rawArgs[i] + " ";
+                }
+                return args;
+            }
+        }
+
+        public static void Usage(string msg = "")
+        {
+            Console.WriteLine(msg);
+            Console.WriteLine(
+@"
+fansc is used to install or uninstall a Fantom application as a Windows service:
+
+  fansc.exe install <ServiceName> <path\to\app\app_launcher.bat> [app arguments]
+
+  fansc.exe uninstall <ServiceName>
+
+  Examples:
+    fansc.exe install MyService C:\fantom\myapp\bin\myservice.bat -port 8080
+    fansc.exe uninstall MyService
+"
+            );
+            Environment.Exit(1);
+        }
+
+        public static void Main(string[] args)
+        {
+            if (args.Length == 0) Usage();
+            new FanSc(args).Run();
+        }
+    }
+}
---
+++ src/fansc/FanSc/Win32Services.cs
@@ -0,0 +1,91 @@
+﻿//
+// Copyright (c) 2015, Brian Frank and Andy Frank
+// Licensed under the Academic Free License version 3.0
+//
+// History:
+//   31 Jul 15  Matthew Giannini    Creation
+//
+
+using System;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Security.AccessControl;
+using System.ServiceProcess;
+
+namespace FanSc
+{
+    internal static class Win32Services
+    {
+        private enum SERVICE_CONFIG_INFO
+        {
+            DESCRIPTION = 1,
+            FAILURE_ACTIONS = 2,
+            DELAYED_AUTO_START_INFO = 3,
+            FAILURE_ACTIONS_FLAG = 4,
+            SERVICE_SID_INFO = 5,
+            REQUIRED_PRIVILEGES_INFO = 6,
+            PRESHUTDOWN_INFO = 7
+        }
+
+        private enum SC_ACTION_TYPE : uint
+        {
+            SC_ACTION_NONE = 0x00000000, // No action.
+            SC_ACTION_RESTART = 0x00000001, // Restart the service.
+            SC_ACTION_REBOOT = 0x00000002, // Reboot the computer.
+            SC_ACTION_RUN_COMMAND = 0x00000003 // Run a command.
+        }
+
+        private static class Win32
+        {
+            [DllImport("advapi32.dll", SetLastError = true)]
+            public static extern bool SetServiceObjectSecurity(SafeHandle serviceHandle,
+                                                                SecurityInfos secInfos,
+                                                                [In] byte[] lpSecDesrBuf);
+
+            [DllImport("advapi32.dll", SetLastError = true)]
+            public static extern bool QueryServiceObjectSecurity(SafeHandle serviceHandle,
+                                                                  SecurityInfos secInfo,
+                                                                  [Out] byte[] lpSecDesrBuf, uint bufSize,
+                                                                  out uint bufSizeNeeded);
+
+            [DllImport("advapi32.dll", EntryPoint = "ChangeServiceConfig2W", ExactSpelling = true,
+                CharSet = CharSet.Unicode, SetLastError = true)]
+            private static extern int ChangeServiceConfig2(SafeHandle hService, int dwInfoLevel, IntPtr lpInfo);
+
+            [DllImport("advapi32.dll", EntryPoint = "ChangeServiceConfigW", ExactSpelling = true,
+                CharSet = CharSet.Unicode, SetLastError = true)]
+            public static extern int ChangeServiceConfig(SafeHandle hService, int nServiceType, int nStartType,
+                                                          int nErrorControl,
+                                                          String lpBinaryPathName, String lpLoadOrderGroup,
+                                                          IntPtr lpdwTagId, [In] String lpDependencies,
+                                                          String lpServiceStartName,
+                                                          String lpPassword, String lpDisplayName);
+
+            public static void SetServiceConfig<T>(ServiceController sc, SERVICE_CONFIG_INFO infoId, T objData)
+            {
+                GCHandle hdata = GCHandle.Alloc(objData, GCHandleType.Pinned);
+                try
+                {
+                    if (0 == ChangeServiceConfig2(sc.ServiceHandle, (int)infoId, hdata.AddrOfPinnedObject()))
+                        throw new Win32Exception();
+                }
+                finally
+                {
+                    hdata.Free();
+                }
+            }
+        }
+
+        internal static void SetServiceExeArgs(ServiceController sc, string exePath, string arguments)
+        {
+            exePath = Path.GetFullPath(exePath.Trim(' ', '\'', '"'));
+            string fqExec = String.Format("\"{0}\" {1}", exePath, arguments).TrimEnd();
+
+            const int notChanged = -1;
+            if (0 == Win32.ChangeServiceConfig(sc.ServiceHandle, notChanged, notChanged, notChanged, fqExec,
+                                               null, IntPtr.Zero, null, null, null, null))
+                throw new Win32Exception();
+        }
+    }
+}
