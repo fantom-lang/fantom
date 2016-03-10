32115ba27599d786846991521f151290ff934c9c
Andy Frank <1316916+afrankvt@users.noreply.github.com>
2016-03-10 09:51:22 -0500

    dom: fix WeakMap.ctor

--- src/dom/js/WeakMapPeer.js
+++ src/dom/js/WeakMapPeer.js
@@ -8,7 +8,7 @@
 
 fan.dom.WeakMapPeer = fan.sys.Obj.$extend(fan.sys.Obj);
 
-fan.dom.WeakMapPeer.prototype.$ctor = function(self) { this.map = WeakMap(); }
+fan.dom.WeakMapPeer.prototype.$ctor = function(self) { this.map = new WeakMap(); }
 fan.dom.WeakMapPeer.prototype.has = function(self, key) { return this.map.has(key); }
 fan.dom.WeakMapPeer.prototype.get = function(self, key) { return this.map.get(key); }
 fan.dom.WeakMapPeer.prototype.set = function(self, key, val) { this.map.set(key, val); return self; }
