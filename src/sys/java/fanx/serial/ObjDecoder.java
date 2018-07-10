a8884185a65c6e0c61ed7d89f26f013e8f48183d
Brian Frank <58821032+briansfrank@users.noreply.github.com>
2018-07-10 09:53:50 -0400

    sys: fix ObjDecoder it-block ctor to only apply when type is const

--- src/sys/java/fanx/serial/ObjDecoder.java
+++ src/sys/java/fanx/serial/ObjDecoder.java
@@ -235,7 +235,7 @@ public class ObjDecoder
       // if last parameter is an function then pass toSet
       // as an it-block for setting the fields
       Param p = (Param)makeCtor.params().last();
-      if (p != null && p.type().fits(Sys.FuncType))
+      if (p != null && p.type().fits(Sys.FuncType) && t.isConst())
       {
         if (args == null) args = new List(Sys.ObjType);
         args.add(Field.makeSetFunc(toSet));
