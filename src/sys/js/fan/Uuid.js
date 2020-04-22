cadc3ae459a3c1eeb9fd00965d8b3988f19e75ab
Matthew Giannini <matthewg.dev@gmail.com>
2020-04-22 19:59:27 -0400

    sys: remove debug

--- src/sys/js/fan/Uuid.js
+++ src/sys/js/fan/Uuid.js
@@ -38,7 +38,6 @@ fan.sys.Uuid.make = function()
   }
   else
   {
-    console.log("boo-ya");
     uuid = ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
       (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
     );
