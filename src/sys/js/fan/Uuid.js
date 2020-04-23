1d48e8306be2c05529a7483cdccced51557e67c9
Matthew Giannini <matthewg.dev@gmail.com>
2020-04-23 16:27:20 -0400

    sys: remove lambda from Uuid.make

--- src/sys/js/fan/Uuid.js
+++ src/sys/js/fan/Uuid.js
@@ -28,7 +28,7 @@ fan.sys.Uuid.prototype.$ctor = function ()
 fan.sys.Uuid.make = function()
 {
   var uuid;
-  if (crypto === undefined)
+  if (window.crypto === undefined)
   {
     // IE
     uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
@@ -38,9 +38,9 @@ fan.sys.Uuid.make = function()
   }
   else
   {
-    uuid = ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-    );
+    uuid = ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, function(c) {
+      return (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16);
+    });
   }
   return fan.sys.Uuid.fromStr(uuid);
 }
