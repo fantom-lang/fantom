2e5a944135b556601cd1a79a3ac900f851bca42c
Matthew Giannini <matthewg.dev@gmail.com>
2020-07-28 16:12:16 -0400

    sys: fix ObjDecoder.readType bug (code review)

--- src/sys/js/fanx/ObjDecoder.js
+++ src/sys/js/fanx/ObjDecoder.js
@@ -549,7 +549,7 @@ fanx_ObjDecoder.prototype.readType = function(lbracket)
   if (this.curt == fanx_Token.COLON)
   {
     this.consume();
-    var lbracket2 = curt == fanx_Token.LBRACKET;
+    var lbracket2 = this.curt == fanx_Token.LBRACKET;
     if (lbracket2) this.consume();
     t = new fan.sys.MapType(t, this.readType(lbracket2));
     if (lbracket2) this.consume(fanx_Token.RBRACKET, "Expected closeing ']'");
