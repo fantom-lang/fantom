4d95c944523a7ea028ec80a389bc3457cdfc50c6
Matthew Giannini <matthewg.dev@gmail.com>
2021-06-28 09:25:40 -0400

    sys: set default timezone in JS from Intl API

--- src/sys/js/fan/TimeZone.js
+++ src/sys/js/fan/TimeZone.js
@@ -68,9 +68,19 @@ fan.sys.TimeZone.rel = function()
 
 fan.sys.TimeZone.cur = function()
 {
-  // TODO
   if (fan.sys.TimeZone.m_cur == null)
-    fan.sys.TimeZone.m_cur = fan.sys.TimeZone.fromStr("New_York");
+  {
+    try
+    {
+      var name = Intl.DateTimeFormat().resolvedOptions().timeZone.split("/")[1];
+      fan.sys.TimeZone.m_cur = fan.sys.TimeZone.fromStr(name);
+    }
+    catch (error)
+    {
+      console.log(error);
+      fan.sys.TimeZone.m_cur = fan.sys.TimeZone.fromStr("New_York");
+    }
+  }
   return fan.sys.TimeZone.m_cur;
 }
 
