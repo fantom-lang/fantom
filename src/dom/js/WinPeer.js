ab31b50b9d9df49844abf54d53609f4d9224784e
Andy Frank <afrankvt@gmail.com>
2021-11-03 15:33:58 -0400

    dom: workaround for Win.hyperlink and mailto: links and +/space encoding

--- src/dom/js/WinPeer.js
+++ src/dom/js/WinPeer.js
@@ -184,7 +184,15 @@ fan.dom.WinPeer.prototype.uri = function(self)
 
 fan.dom.WinPeer.prototype.hyperlink = function(self, uri)
 {
-  this.win.location = uri.encode();
+  var href = uri.encode();
+  if (uri.m_scheme == "mailto")
+  {
+    // TODO: mailto links are not decoding + as spaces properly, so
+    // not showing up correctly in email clients when subj/body are
+    // specified; for now just manually convert back
+    href = href.replaceAll("+", " ");
+  }
+  this.win.location = href;
 }
 
 fan.dom.WinPeer.prototype.reload  = function(self, force)
