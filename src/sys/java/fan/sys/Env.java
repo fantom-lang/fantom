594460e611f05eb684860e878a83efa7f54a0240
Brian Frank <briansfrank@gmail.com>
2022-11-04 12:08:57 -0400

    sys: fix Env.config to route to Env.props
    This lets JarDistEnv correctly handle props correctly

--- src/sys/java/fan/sys/Env.java
+++ src/sys/java/fan/sys/Env.java
@@ -184,7 +184,7 @@ public abstract class Env
   public String config(Pod pod, String key) { return config(pod, key, null); }
   public String config(Pod pod, String key, String def)
   {
-    return (String)props.get(pod, configProps, Duration.oneMin).get(key, def);
+    return (String)props(pod, configProps, Duration.oneMin).get(key, def);
   }
 
   public String locale(Pod pod, String key) { return locale(pod, key, noDef, Locale.cur()); }
