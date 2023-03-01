50b529865fddc0a9d188edaacbf4dac9b91064ac
Jeremy Criquet <thekeydesign@gmail.com>
2023-03-01 04:35:31 -0800

    BigInt arithmetic (#19)
    * math BigInt: [Formatting] Converted tabs to spaces
    
    * math BigInt: Documented existing methods and fixed minor typos & inconsistent formatting
    
    * math BigInt: Added many of the other common java BigInteger methods
    
    ---------
    
    Co-authored-by: Jeremy C <jeremyc@j2inn.com>

--- src/math/fan/BigInt.fan
+++ src/math/fan/BigInt.fan
@@ -4,6 +4,7 @@
 //
 // History:
 //   06 Aug 2021 Matthew Giannini Creation
+//   26 Feb 2023 Jeremy Criquet Added many of the common methods from java's BigInteger
 //
 
 **
@@ -16,81 +17,193 @@ native const class BigInt
 // Construction
 //////////////////////////////////////////////////////////////////////////
 
-	static new fromStr(Str s, Int radix := 10, Bool checked := true)
+  ** Parse a Str into a BigInt using the specified radix.
+  ** If invalid format and checked is false return null,
+  ** otherwise throw ParseErr.
+  static new fromStr(Str s, Int radix := 10, Bool checked := true)
 
-	** Default value is 0.
-	static const BigInt defVal
-	static const BigInt zero
-	static const BigInt one
+  ** Default value is 0.
+  static const BigInt defVal
+  static const BigInt zero
+  static const BigInt one
 
-	new makeInt(Int val)
-	new makeBuf(Buf bytes)
+  ** Returns a BigInt whose value is equal to that of
+  ** the specified Int. 
+  new makeInt(Int val)
 
-	private new privateMake()
+  ** Translates a byte array containing the two's-complement binary
+  ** representation of a BigInt into a BigInt.
+  new makeBuf(Buf bytes)
+
+  ** Private constructor.
+  private new privateMake()
 
 //////////////////////////////////////////////////////////////////////////
 // Obj
 //////////////////////////////////////////////////////////////////////////
 
-	override Bool equals(Obj? obj)
+  ** Return true if same both represent that same integer value.
+  override Bool equals(Obj? obj)
 
-	override Int compare(Obj obj)
+  ** Compare based on integer value.
+  override Int compare(Obj obj)
 
-	override Str toStr()
-
-//////////////////////////////////////////////////////////////////////////
-// Identity
-//////////////////////////////////////////////////////////////////////////
-
-	** -1, 0, 1 if the BigInt is negative, zero, or positive.
-	Int signum()
-
-	** Convert the number to an Int.
-	**
-	** If the value is out-of-range and checked is true, an Err is thrown.
-	** Otherwise the value is truncated, with possible loss of sign.
-	Int toInt(Bool checked := true)
-
-	Buf toBuf()
+  ** The hash for a BigInt is platform dependent.
+  override Int hash()
 
 //////////////////////////////////////////////////////////////////////////
-// Methods
+// Operations
 //////////////////////////////////////////////////////////////////////////
 
   ////////// unary //////////
 
-	@Operator BigInt negate()
+  ** Negative of this.  Shortcut is -a.
+  @Operator BigInt negate()
 
-	@Operator BigInt increment()
+  ** Increment by one.  Shortcut is ++a or a++.
+  @Operator BigInt increment()
 
-	@Operator BigInt decrement()
+  ** Decrement by one.  Shortcut is --a or a--.
+  @Operator BigInt decrement()
+
+  ////////// mult //////////
+
+  ** Multiply this with b.  Shortcut is a*b.
+  @Operator BigInt mult(BigInt b)
+
+  ** Multiply this with b.  Shortcut is a*b.
+  @Operator BigInt multInt(Int b)
+
+  ////////// div //////////
+
+  ** Divide this by b.  Shortcut is a/b.
+  @Operator BigInt div(BigInt b)
+
+  ** Divide this by b.  Shortcut is a/b.
+  @Operator BigInt divInt(Int b)
+
+  ////////// mod //////////
+
+  ** Return remainder of this divided by b.  Shortcut is a%b.
+  @Operator BigInt mod(BigInt b)
+
+  ** Return remainder of this divided by b.  Shortcut is a%b.
+  @Operator Int modInt(Int b)
+
+  ////////// plus //////////
+
+  ** Add this with b.  Shortcut is a+b.
+  @Operator BigInt plus(BigInt b)
+
+  ** Add this with b.  Shortcut is a+b.
+  @Operator BigInt plusInt(Int b)
+
+  ////////// minus //////////
+
+  ** Subtract b from this.  Shortcut is a-b.
+  @Operator BigInt minus(BigInt b)
+
+  ** Subtract b from this.  Shortcut is a-b.
+  @Operator BigInt minusInt(Int b)
 
 //////////////////////////////////////////////////////////////////////////
 // Bitwise
 //////////////////////////////////////////////////////////////////////////
 
-	BigInt setBit(Int b)
+  ** Set the given bit to 1.
+  ** Equivalent to this.or(1.shiftl(b)).
+  BigInt setBit(Int b)
 
-	BigInt clearBit(Int b)
+  ** Set the given bit to 0.
+  ** Equivalent to this.and(1.shiftl(b).not).
+  BigInt clearBit(Int b)
 
-	BigInt flipBit(Int b)
+  ** Flip the given bit between 0 and 1.
+  ** Equivalent to this.xor(1.shiftl(b)).
+  BigInt flipBit(Int b)
 
-	Bool testBit(Int b)
+  ** Return true if given bit is 1.
+  ** Equivalent to this.and(1.shiftl(b)) != 0.
+  Bool testBit(Int b)
 
-	** Returns the number of bits in the minimal two's-complement
-	** representation of this BigInteger, excluding a sign bit.
-	Int bitLen()
+  ** Returns the number of bits in the minimal two's-complement
+  ** representation of this BigInteger, excluding a sign bit.
+  Int bitLen()
 
-	BigInt not()
+  ** Bitwise not/inverse of this.
+  BigInt not()
 
-	BigInt and(Obj b)
+  ** Bitwise-and of this and b.
+  BigInt and(Obj b)
 
-	BigInt or(Obj b)
+  ** Bitwise-or of this and b.
+  BigInt or(Obj b)
 
-	BigInt xor(Obj b)
+  ** Bitwise-exclusive-or of this and b.
+  BigInt xor(Obj b)
 
-	BigInt shiftl(Int b)
+  ** Bitwise left shift of this by b.
+  ** Negative values call shiftr instead.
+  BigInt shiftl(Int b)
 
-	BigInt shftr(Int b)
+  ** Bitwise arithmetic right-shift of this by b.  Note that this is
+  ** similar to Int.shifta, not Int.shiftr.  Sign extension is performed.
+  ** Negative values call shiftl instead.
+  BigInt shiftr(Int b)
+
+//////////////////////////////////////////////////////////////////////////
+// Math
+//////////////////////////////////////////////////////////////////////////
+
+  ** -1, 0, 1 if the BigInt is negative, zero, or positive.
+  Int signum()
+
+  ** Return the absolute value of this integer.  If this value is
+  ** positive then return this, otherwise return the negation.
+  BigInt abs()
+
+  ** Return the smaller of this and the specified BigInt values.
+  BigInt min(BigInt that)
+
+  ** Return the larger of this and the specified BigInt values.
+  BigInt max(BigInt that)
+
+  ** Return this value raised to the specified power.
+  ** Throw ArgErr if pow is less than zero.
+  BigInt pow(Int pow)
+
+//////////////////////////////////////////////////////////////////////////
+// Conversion
+//////////////////////////////////////////////////////////////////////////
+
+  ** Convert the number to an Int.
+  **
+  ** If the value is out-of-range and checked is true, an Err is thrown.
+  ** Otherwise the value is truncated, with possible loss of sign.
+  Int toInt(Bool checked := true)
+
+  ** Convert the number to an Float.
+  **
+  ** If the value is out-of-range, it will return Float.posInf
+  ** or Float.negInf.  Possible loss of precision is still possible, even
+  ** if the value is finite.
+  Float toFloat()
+
+  ** Convert the number to an Decimal.
+  **
+  ** This simply wraps the BigInt with Decimal with a 0 scale,
+  ** equivilent mathematically to int * 2^0
+  Decimal toDecimal()
+
+  ** Returns a byte array containing the two's-complement representation
+  ** of this BigInt.
+  Buf toBuf()
+
+  ** Return decimal string representation.
+  override Str toStr()
+
+  ** Return string representation in given radix.  If width is non-null,
+  ** then leading zeros are prepended to ensure the specified width.
+  Str toRadix(Int radix, Int? width := null)
 
 }
--- src/math/java/BigInt.java
+++ src/math/java/BigInt.java
@@ -8,6 +8,7 @@
 
 package fan.math;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
 import fan.sys.*;
 
@@ -18,35 +19,35 @@ public final class BigInt extends FanObj
 // Construction
 //////////////////////////////////////////////////////////////////////////
 
-	public static BigInt fromStr(final String s) { return fromStr(s, 10); }
-	public static BigInt fromStr(final String s, final long radix) { return fromStr(s, radix, true); }
-	public static BigInt fromStr(final String s, final long radix, final boolean checked)
-	{
-		try
-		{
-			return new BigInt(new BigInteger(s, (int)radix));
-		}
-		catch (NumberFormatException e)
-		{
-			if (!checked) return null;
-			throw ParseErr.make("BigInt", s);
-		}
-	}
+  public static BigInt fromStr(final String s) { return fromStr(s, 10); }
+  public static BigInt fromStr(final String s, final long radix) { return fromStr(s, radix, true); }
+  public static BigInt fromStr(final String s, final long radix, final boolean checked)
+  {
+    try
+    {
+      return new BigInt(new BigInteger(s, (int)radix));
+    }
+    catch (NumberFormatException e)
+    {
+      if (!checked) return null;
+      throw ParseErr.make("BigInt", s);
+    }
+  }
 
-	public static BigInt makeInt(final long val)
-	{
-		return new BigInt(BigInteger.valueOf(val));
-	}
+  public static BigInt makeInt(final long val)
+  {
+    return new BigInt(BigInteger.valueOf(val));
+  }
 
-	public static BigInt makeBuf(Buf bytes)
-	{
-		return new BigInt(new BigInteger(bytes.safeArray()));
-	}
+  public static BigInt makeBuf(Buf bytes)
+  {
+    return new BigInt(new BigInteger(bytes.safeArray()));
+  }
 
-	public BigInt(BigInteger val)
-	{
-		this.self = val;
-	}
+  public BigInt(BigInteger val)
+  {
+    this.self = val;
+  }
 
 //////////////////////////////////////////////////////////////////////////
 // Type
@@ -56,29 +57,6 @@ public final class BigInt extends FanObj
   private static final Type typeof = Type.find("math::BigInt");
 
 //////////////////////////////////////////////////////////////////////////
-// Identity
-//////////////////////////////////////////////////////////////////////////
-
- 	public static final BigInt defVal = new BigInt(BigInteger.ZERO);
- 	public static final BigInt zero   = defVal;
- 	public static final BigInt one    = new BigInt(BigInteger.ONE);
-
-	public long signum() { return self.signum(); }
-
-	public long toInt() { return toInt(true); }
-	public long toInt(final boolean checked)
-	{
-		// longValueExact() is 1.8 only, but preferable
-		return self.longValue();
-//		return checked ? self.longValueExact() : self.longValue();
-	}
-
-	public Buf toBuf()
-	{
-		return new MemBuf(self.toByteArray());
-	}
-
-//////////////////////////////////////////////////////////////////////////
 // Operators
 //////////////////////////////////////////////////////////////////////////
 
@@ -86,48 +64,113 @@ public final class BigInt extends FanObj
 
   public BigInt increment() { return new BigInt(self.add(BigInteger.ONE)); }
 
- 	public BigInt decrement() { return new BigInt(self.subtract(BigInteger.ONE)); }
+  public BigInt decrement() { return new BigInt(self.subtract(BigInteger.ONE)); }
+
+  public BigInt mult(BigInt x) { return new BigInt(self.multiply(x.self)); }
+  public BigInt multInt(long x) { return new BigInt(self.multiply(BigInteger.valueOf(x))); }
+
+  public BigInt div(BigInt x) { return new BigInt(self.divide(x.self)); }
+  public BigInt divInt(long x) { return new BigInt(self.divide(BigInteger.valueOf(x))); }
+
+  public BigInt mod(BigInt x) { return new BigInt(self.remainder(x.self)); }
+  public long modInt(long x) { return self.remainder(BigInteger.valueOf(x)).longValue(); }
+
+  public BigInt plus(BigInt x) { return new BigInt(self.add(x.self)); }
+  public BigInt plusInt(long x) { return new BigInt(self.add(BigInteger.valueOf(x))); }
+
+  public BigInt minus(BigInt x) { return new BigInt(self.subtract(x.self)); }
+  public BigInt minusInt(long x) { return new BigInt(self.subtract(BigInteger.valueOf(x))); }
 
 //////////////////////////////////////////////////////////////////////////
 // Bitwise
 //////////////////////////////////////////////////////////////////////////
 
- 	public BigInt setBit(final long bit) { return new BigInt(self.setBit((int)bit)); }
+  public BigInt setBit(final long bit) { return new BigInt(self.setBit((int)bit)); }
 
- 	public BigInt clearBit(final long bit) { return new BigInt(self.clearBit((int)bit)); }
+  public BigInt clearBit(final long bit) { return new BigInt(self.clearBit((int)bit)); }
 
- 	public BigInt flipBit(final long bit) { return new BigInt(self.flipBit((int)bit)); }
+  public BigInt flipBit(final long bit) { return new BigInt(self.flipBit((int)bit)); }
 
- 	public boolean testBit(final long bit) { return self.testBit((int)bit); }
+  public boolean testBit(final long bit) { return self.testBit((int)bit); }
 
- 	public long bitLen() { return self.bitLength(); }
+  public long bitLen() { return self.bitLength(); }
 
- 	public BigInt not() { return new BigInt(self.not()); }
+  public BigInt not() { return new BigInt(self.not()); }
 
- 	public BigInt and(Object b) { return new BigInt(self.and(bi(b))); }
+  public BigInt and(Object b) { return new BigInt(self.and(bi(b))); }
 
- 	public BigInt or(Object b) { return new BigInt(self.or(bi(b))); }
+  public BigInt or(Object b) { return new BigInt(self.or(bi(b))); }
 
- 	public BigInt xor(Object b) { return new BigInt(self.xor(bi(b))); }
+  public BigInt xor(Object b) { return new BigInt(self.xor(bi(b))); }
 
- 	public BigInt shiftl(long b) { return new BigInt(self.shiftLeft((int)b)); }
+  public BigInt shiftl(long b) { return new BigInt(self.shiftLeft((int)b)); }
 
- 	public BigInt shiftr(long b) { return new BigInt(self.shiftRight((int)b)); }
+  public BigInt shiftr(long b) { return new BigInt(self.shiftRight((int)b)); }
 
- 	private static BigInteger bi(Object obj)
- 	{
- 		if (obj instanceof BigInt) return ((BigInt)obj).self;
- 		if (obj instanceof Long) return BigInteger.valueOf((Long)obj);
- 		throw ArgErr.make("Not a BigInt or Int: " + obj.getClass());
- 	}
+  private static BigInteger bi(Object obj)
+  {
+    if (obj instanceof BigInt) return ((BigInt)obj).self;
+    if (obj instanceof Long) return BigInteger.valueOf((Long)obj);
+    throw ArgErr.make("Not a BigInt or Int: " + obj.getClass());
+  }
 
 //////////////////////////////////////////////////////////////////////////
 // Math
 //////////////////////////////////////////////////////////////////////////
 
-  public BigInt abs()
+  public long signum() { return self.signum(); }
+
+  public BigInt abs() { return new BigInt(self.abs()); }
+
+  public BigInt min(BigInt x) { return new BigInt(self.min(x.self)); }
+
+  public BigInt max(BigInt x) { return new BigInt(self.max(x.self)); }
+
+  public BigInt pow(long x)
   {
-  	return new BigInt(self.abs());
+    int asInt = x > (long)Integer.MAX_VALUE
+      ? Integer.MAX_VALUE
+      : (x < (long)Integer.MIN_VALUE
+        ? Integer.MIN_VALUE
+        : (int)x);
+    return new BigInt(self.pow(asInt));
+  }
+
+//////////////////////////////////////////////////////////////////////////
+// Conversion
+//////////////////////////////////////////////////////////////////////////
+
+  public long toInt() { return toInt(true); }
+  public long toInt(final boolean checked)
+  {
+    // longValueExact() is 1.8 only, but preferable
+    return checked ? self.longValueExact() : self.longValue();
+  }
+
+  public double toFloat() { return self.floatValue(); }
+
+  public BigDecimal toDecimal() { return new BigDecimal(self); }
+
+  public Buf toBuf()
+  {
+    return new MemBuf(self.toByteArray());
+  }
+
+  public String toRadix(long radix) { return toRadix(radix, null); }
+  public String toRadix(long radix, Long width)
+  {
+    return pad(self.toString((int)radix), width);
+  }
+
+  // Taken from FanInt.java
+  private static String pad(String s, Long width)
+  {
+    if (width == null || s.length() >= width.intValue()) return s;
+    StringBuilder sb = new StringBuilder(width.intValue());
+    int zeros = width.intValue() - s.length();
+    for (int i=0; i<zeros; ++i) sb.append('0');
+    sb.append(s);
+    return sb.toString();
   }
 
 //////////////////////////////////////////////////////////////////////////
@@ -136,33 +179,37 @@ public final class BigInt extends FanObj
 
   public boolean equals(Object obj)
   {
-  	if (obj instanceof BigInt)
-  	{
-  		BigInt that = (BigInt)obj;
-  		return self.equals(that.self);
-  	}
-  	return false;
+    if (obj instanceof BigInt)
+    {
+      BigInt that = (BigInt)obj;
+      return self.equals(that.self);
+    }
+    return false;
   }
 
   public long compare(Object obj)
   {
-  	BigInt that = (BigInt)obj;
-  	return self.compareTo(that.self);
+    BigInt that = (BigInt)obj;
+    return self.compareTo(that.self);
   }
 
   public long hash()
   {
-  	return self.hashCode();
+    return self.hashCode();
   }
 
   public String toStr()
   {
-  	return self.toString();
+    return self.toString();
   }
 
 //////////////////////////////////////////////////////////////////////////
 // Fields
 //////////////////////////////////////////////////////////////////////////
+
+  public static final BigInt defVal = new BigInt(BigInteger.ZERO);
+  public static final BigInt zero   = defVal;
+  public static final BigInt one    = new BigInt(BigInteger.ONE);
 
   private BigInteger self;
 
