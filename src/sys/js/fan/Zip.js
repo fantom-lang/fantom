6d9e1f47303eed2ea830c99c87bf260b1e6d27b5
Matthew Giannini <Matthew Giannini 1274158+mgiannini@users.noreply.github.com>
2023-03-21 09:40:07 -0400

    sys:js - stub out Zip.js

---
+++ src/sys/js/fan/Zip.js
@@ -0,0 +1,15 @@
+//
+// Copyright (c) 2023, Brian Frank and Andy Frank
+// Licensed under the Academic Free License version 3.0
+//
+// History:
+//   21 Mar 2023  Matthew Giannini  Creation
+//
+
+/**
+ * Zip.
+ */
+fan.sys.Zip = fan.sys.Obj.$extend(fan.sys.Obj);
+
+fan.sys.Zip.prototype.$ctor = function() {}
+fan.sys.Zip.prototype.$typeof = function() { return fan.util.Zip.$type; }
