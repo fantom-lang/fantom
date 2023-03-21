43ff121a4777e97d5e12fa8987458cabed092978
Matthew Giannini <Matthew Giannini 1274158+mgiannini@users.noreply.github.com>
2023-03-21 10:14:55 -0400

    concurrent: add constructor impl for ActorPool.js

--- src/concurrent/js/ActorPool.js
+++ src/concurrent/js/ActorPool.js
@@ -12,6 +12,24 @@
  */
 fan.concurrent.ActorPool = fan.sys.Obj.$extend(fan.sys.Obj);
 
-fan.concurrent.ActorPool.prototype.$ctor = function() {}
+fan.concurrent.ActorPool.prototype.$ctor = function()
+{
+  fan.sys.Obj.prototype.$ctor.call(this);
+  this.m_$name = "ActorPool";
+  this.m_$maxThreads = 100;
+}
+
 fan.concurrent.ActorPool.prototype.$typeof = function() { return fan.concurrent.ActorPool.$type; }
 
+fan.concurrent.ActorPool.make = function(f)
+{
+  var self = new fan.concurrent.ActorPool();
+  fan.concurrent.ActorPool.make$(self, f);
+  return self;
+}
+
+fan.concurrent.ActorPool.make$ = function(self, f)
+{
+  if (f === undefined) f = null;
+  if (f != null) f.call(self);
+}
