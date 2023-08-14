be8b75779bf2671ded70de17c9f39c837e000242
Matthew Giannini <Matthew Giannini 1274158+mgiannini@users.noreply.github.com>
2023-08-14 11:07:56 -0400

    util: port ES JavaScript

---
+++ src/util/es/SecureRandomPeer.js
@@ -0,0 +1,11 @@
+//
+// Copyright (c) 2023, Brian Frank and Andy Frank
+// Licensed under the Academic Free License version 3.0
+//
+// History:
+//   17 Mar 2023  Matthew Giannini  Creation
+//
+
+/**
+ * SecureRandom.
+ */
